ec4fd499c6e7cb426e18f3b7fe8db402e563d139
yilmaz <ydaskafa@gmail.com>
2022-02-16 23:53:23 +0300

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<Sidebar />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Sidebar from '@/components/Sidebar'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    components: {
+        Sidebar
+    }
 }
 </script>
